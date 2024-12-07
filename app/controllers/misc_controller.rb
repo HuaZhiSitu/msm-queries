da9c9966a373a6a7a16af592c77049358c539e7f
HuaZhiSitu <hsitu@chicagobooth.edu>
2024-12-07 09:14:30 +0000

    finished. included for loops and matched many tables

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,51 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def alldirectors
+    @directors = Director.all
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def youngestdirector
+    @youngest_director = Director.where.not(dob: nil).order(dob: :desc).first
+
+    render({ :template => "misc_templates/youngest"})
+  end
+
+  def eldestdirector
+    @eldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+    render({ :template => "misc_templates/eldest"})
+  end
+
+  def identification
+    @director = Director.find(params[:the_id])
+    @movies=Movie.all.index_by(&:id)
+    render({:template=>"misc_templates/director"})
+  end
+
+  def allmovies
+    @movies = Movie.all
+    @directorid = Director.all.index_by(&:id)
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def moviesidentification
+    @movies=Movie.find(params[:id])
+    @directorid = Director.all.index_by(&:id)
+    render({:template=>"misc_templates/movie"})
+  end
+
+  def allactors
+    @actors=Actor.all
+    render({ :template => "misc_templates/actors"})
+  end
+
+  def actoridentification
+    @actors = Actor.find(params[:id])
+    @directorid = Director.all.index_by(&:id)
+    @movies=Movie.all.index_by(&:id)
+    render({:template=>"misc_templates/actor"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "alldirectors" })
+  get("/directors/youngest", { :controller => "misc", :action => "youngestdirector" })
+  get("/directors/eldest", { :controller => "misc", :action => "eldestdirector" })
+  get("/directors/:the_id", {:controller=>"misc",:action=>"identification"})
+  get("/movies", { :controller => "misc", :action => "allmovies" })
+  get("/movies/:id", {:controller=>"misc",:action=>"moviesidentification"})
+  get("/actors", { :controller => "misc", :action => "allactors" })
+  get("/actors/:id",{:controller=>"misc",:action=>"actoridentification"})
 end
